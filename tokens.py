e64b4dd551be99b3497587bcdf021a0d94c4fef8
phlakhani <60407265+phlakhani@users.noreply.github.com>
2020-03-06 17:59:38 -0500

    Update and rename twitter_tokens.py to tokens.py

---
+++ tokens.py
@@ -0,0 +1,8 @@
+##this is confidential but i dont really use twitter i made twitter accound jut for this application
+#so now this tokens are really unworthy !
+
+
+ACCESS_TOKEN="1198790338846101509-uYKaX9fvFaRSx84GLYzsvRyaHvXNM0"
+ACCESS_TOKEN_SECRET="0WMIXvOvkAJKTQnjHabtE8mcY36RR6grExLrvq7FlhGNp"
+CONSUMER_KEY="P6TpWEjASJbfohVUoSDAMJvFi"
+CONSUMER_SECRET="bos8dJFNbKWWly0dKopA0RzZF5h4iq6PPjd9RSNuwzILIQr5V4"
