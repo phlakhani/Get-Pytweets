64f995f81a46769e1a6ec91abf59fc19c91d6940
Piyush Lakhani <60407265+phlakhani@users.noreply.github.com>
2020-07-16 12:37:43 -0400

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,8 +1,5 @@
-##this is confidential but i dont really use twitter i made twitter accound jut for this application
-#so now this tokens are really unworthy !
 
-
-ACCESS_TOKEN="1198790338846101509-uYKaX9fvFaRSx84GLYzsvRyaHvXNM0"
-ACCESS_TOKEN_SECRET="0WMIXvOvkAJKTQnjHabtE8mcY36RR6grExLrvq7FlhGNp"
-CONSUMER_KEY="P6TpWEjASJbfohVUoSDAMJvFi"
-CONSUMER_SECRET="bos8dJFNbKWWly0dKopA0RzZF5h4iq6PPjd9RSNuwzILIQr5V4"
+ACCESS_TOKEN="1198790338846101509-####################3"
+ACCESS_TOKEN_SECRET="0WMIXvOvkAJKTQnjHab###################3"
+CONSUMER_KEY="P6TpWEjASJb############3"
+CONSUMER_SECRET="bos8dJFNbKWWly0dK###############3"
